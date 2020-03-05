7aba3b1b0e57ea897ad72be7ddaff94972d9108e
Ruben J. Sibon <mail@webricolage.nl>
2020-03-05 13:18:02 +0100

    Print out num days and dates

--- firstSpringDay.js
+++ firstSpringDay.js
@@ -106,8 +106,20 @@
     const lineStyles = { margin: "0 1px", width: "8px", height: 0 };
     const getFirstSpringDayPerYear = firstSpringDayPerEachYear(splitDatasetIntoYears(dataOfYear));
 
+    // @todo Create table with data ("Number of days since January 1st" and "Date of first spring day" in two columns)
+    // const nodeTable = document.createElement("table");
+    // const nodeThead = document.createElement("thead");
+    // const nodeTh = document.createElement("th");
+    // const nodeTh1 = nodeTh;
+    // const nodeTh2 = nodeTh;
+
     let lineData = [];
     let axisYDays = 0;
+
+    // nodeTh1.innerHTML = "DAGEN T/M";
+    // nodeTh1.innerHTML = "DATUM";
+    // nodeThead.innerHTML = nodeTh;
+    // nodeTable.innerHTML = nodeThead;
 
     // Make and append a new bar for each data point.
     for (let index = 0; index < getFirstSpringDayPerYear.length; index++) {
@@ -116,6 +128,8 @@
       const nodeLine = document.createElement("hr");
       const nodeYear = document.createElement("span");
       const nodeLog = document.createElement("p");
+      const nodeTr = document.createElement("tr");
+      const nodeDataTd = document.createElement("td");
 
       const year = firstSpringDay.getFullYear();
       const month = firstSpringDay.getMonth();
@@ -155,7 +169,9 @@
 
       // Log result to page.
       nodeLog.innerHTML = logResult(firstSpringDay);
+      nodeDataTd.innerHTML = `${value}, ${firstSpringDay.toLocaleDateString("nl-NL")}`;
       $resultsLog.appendChild(nodeLog);
+      $resultsLog.appendChild(nodeDataTd);
 
       // Add the bar to the chart.
       $barchart.appendChild(nodeLine);
