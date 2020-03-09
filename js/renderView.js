5048cfdb2caf34e8240c4c13145418f03a6aab87
Ruben J. Sibon <mail@webricolage.nl>
2020-03-09 23:29:58 +0100

    WIP Refactor renderView module into smaller parts

--- js/renderView.js
+++ js/renderView.js
@@ -1,7 +1,84 @@
 import getWeatherData from "./dataHandling.js";
 import { daysInMonth, dataOfYear, splitDatasetIntoYears, firstSpringDayForYear, firstSpringDayPerEachYear, logResult } from "./firstSpringDay.js";
 
+/**
+ * @todo Considering to refactor this render module into a OOP Class.
+**/
+
+// Constants
+const ZOOM_MULTIPLIER = 4;
+const BAR_STYLES = { margin: "0 1px", width: "8px", height: 0 };
+
+// Selected elements
+const $axisY = document.querySelector("#axisY");
+const $barchart = document.querySelector("#barchart");
+
 let tempsPerDay;
+let getFirstSpringDayPerYear;
+
+/**
+ * Render barchart
+**/
+const shadowRenderBars = (barValue, barYear) => {
+  const nodeBar = document.createElement("hr");
+
+  for (const style in BAR_STYLES) {
+    nodeBar.style[style] = BAR_STYLES[style];
+  }
+
+  nodeBar.style.height = 0;
+  nodeBar.setAttribute("title", barValue);
+  nodeBar.setAttribute("data-year", barYear);
+
+  setTimeout(() => {
+    nodeBar.style.height = `${barValue * ZOOM_MULTIPLIER}px`;
+  }, 500);
+
+  return nodeBar;
+};
+
+/**
+ * Render barchart
+**/
+const barchartRender = (bars, axisYLength) => {
+  $barchart.style.width = `${getFirstSpringDayPerYear.length}px;`;
+  $barchart.style.opacity = 1;
+  $barchart.style.setProperty("--barchart-height", `${axisYLength * ZOOM_MULTIPLIER}px`);
+
+  $axisY.innerHTML = `<div>${axisYLength}</div> <div>0</div>`;
+
+  $barchart.append(...bars);
+};
+
+/**
+ * Attach barchart interactivity
+**/
+const barchartInteractivity = () => {
+  $barchart.addEventListener("mouseover", ({ target }) => {
+    if (target && target.title) {
+      console.log(`${target.dataset.year}: ${target.title}`);
+    }
+  });
+};
+
+/**
+ * Render table
+**/
+// @todo Create table with data ("Number of days since January 1st" and "Date of first spring day" in two columns)
+// const nodeTable = document.createElement("table");
+// const nodeThead = document.createElement("thead");
+// const nodeTh = document.createElement("th");
+// const nodeTh1 = nodeTh;
+// const nodeTh2 = nodeTh;
+
+// nodeTh1.innerHTML = "DAGEN T/M";
+// nodeTh1.innerHTML = "DATUM";
+// nodeThead.innerHTML = nodeTh;
+// nodeTable.innerHTML = nodeThead;
+
+/**
+ * Attach table interactivity
+**/
 
 /**
  * Draw the graph and write the logs (side effects)
@@ -9,45 +86,25 @@ let tempsPerDay;
  * @todo Break this monster function up...
 **/
 const drawResults = () => {
-  const ZOOM_MULTIPLIER = 4;
-
-  const $barchart = document.querySelector("#barchart");
   const $axisX = document.querySelector("#axisX");
-  const $axisY = document.querySelector("#axisY");
   const $resultsLog = document.querySelector("#resultsLog");
 
-  const lineStyles = { margin: "0 1px", width: "8px", height: 0 };
-  const getFirstSpringDayPerYear = firstSpringDayPerEachYear(splitDatasetIntoYears(dataOfYear, tempsPerDay));
+  const nodeBars = [];
 
-  // @todo Create table with data ("Number of days since January 1st" and "Date of first spring day" in two columns)
-  // const nodeTable = document.createElement("table");
-  // const nodeThead = document.createElement("thead");
-  // const nodeTh = document.createElement("th");
-  // const nodeTh1 = nodeTh;
-  // const nodeTh2 = nodeTh;
-
-  let lineData = [];
+  let barData = [];
   let axisYDays = 0;
-
-  // nodeTh1.innerHTML = "DAGEN T/M";
-  // nodeTh1.innerHTML = "DATUM";
-  // nodeThead.innerHTML = nodeTh;
-  // nodeTable.innerHTML = nodeThead;
 
   // Make and append a new bar for each data point.
   for (let index = 0; index < getFirstSpringDayPerYear.length; index++) {
     const firstSpringDay = getFirstSpringDayPerYear[index];
 
-    const nodeLine = document.createElement("hr");
-    const nodeYear = document.createElement("span");
-    const nodeLog = document.createElement("p");
     const nodeTr = document.createElement("tr");
-    const nodeDataTd = document.createElement("td");
 
     const year = firstSpringDay.getFullYear();
     const month = firstSpringDay.getMonth();
 
     let value = 0;
+    let nodeBar;
 
     for (let index = month; index > 0; index--) {
       value = value + daysInMonth(year, index);
@@ -58,49 +115,34 @@ const drawResults = () => {
     axisYDays = value >= axisYDays ? value + 10 : axisYDays;
 
     // Add the value of the bar to an array.
-    lineData.push({ 
-      year: firstSpringDay.getFullYear(),
+    barData.push({ 
+      year,
       value,
     });
 
-    // Apply styling to bar.
-    for (const style in lineStyles) {
-      nodeLine.style[style] = lineStyles[style];
-    }
-
-    nodeLine.style.height = 0;
-    nodeLine.setAttribute("title", value);
-    nodeLine.setAttribute("data-year", lineData[index].year);
-
-    setTimeout(() => {
-      nodeLine.style.height = `${value * ZOOM_MULTIPLIER}px`;
-    }, 500);
-
-    const twoDigitYear = lineData[index].year - (lineData[index].year >= 2000 ? 2000 : 1900);
+    // X-axis: years
+    const nodeYear = document.createElement("span");
+    const twoDigitYear = barData[index].year - (barData[index].year >= 2000 ? 2000 : 1900);
     nodeYear.innerHTML = `'${twoDigitYear}`;
     $axisX.appendChild(nodeYear);
 
-    // Log result to page.
+    // Log result to page in a table.
+    const nodeLog = document.createElement("p");
     nodeLog.innerHTML = logResult(firstSpringDay);
+
+    const nodeDataTd = document.createElement("td");
     nodeDataTd.innerHTML = `${value}, ${firstSpringDay.toLocaleDateString("nl-NL")}`;
     $resultsLog.appendChild(nodeLog);
     $resultsLog.appendChild(nodeDataTd);
 
+    nodeBar = shadowRenderBars(value, barData[index].year);
+
     // Add the bar to the chart.
-    $barchart.appendChild(nodeLine);
+    nodeBars.push(nodeBar);
   }
 
-  $barchart.style.width = `${getFirstSpringDayPerYear.length}px;`;
-  $barchart.style.opacity = 1;
-  $barchart.style.setProperty("--barchart-height", `${axisYDays * ZOOM_MULTIPLIER}px`);
-
-  $axisY.innerHTML = `<div>${axisYDays}</div> <div>0</div>`;
-
-  $barchart.addEventListener("mouseover", ({ target }) => {
-    if (target && target.title) {
-      console.log(`${target.dataset.year}: ${target.title}`);
-    }
-  });
+  barchartRender(nodeBars, axisYDays);
+  barchartInteractivity();
 };
 
 /**
@@ -116,7 +158,15 @@ const drawResults = () => {
     alert("An unknown problem occured. ¯\_(ツ)_/¯ Please try again by reloading the page.");
   }
 })().then(data => {
+  /**
+   * Set the data (temperatures per day).
+  **/
   tempsPerDay = data;
+
+  /**
+   * Curry together the first spring day per year.
+  **/
+  getFirstSpringDayPerYear = firstSpringDayPerEachYear(splitDatasetIntoYears(dataOfYear, tempsPerDay));
 
   drawResults();
 });
