e0227fdf92bec3ba0b5c9d3840b4d465ff378a2d
Ruben J. Sibon <mail@webricolage.nl>
2020-03-09 19:56:38 +0100

    Add ES6 Modules and async calls with axios

--- firstSpringDay.js
+++ firstSpringDay.js
@@ -1,194 +1,196 @@
-(function() {
+import getWeatherData from "./dataHandling.js";
+
+let tempsPerDay;
+
+/**
+  * Get the number of days in a given month.
+  *
+  * @param {number} month - Month in MM format (use current month if undefined).
+  * @param {number} year - Year in YYYY format (use current year if undefined).
+  *
+  * @returns {number}
+**/
+const daysInMonth = (year = (new Date().getFullYear()), month = (new Date().getMonth())) => {
+  return new Date(year, month, 0).getDate();
+}
+
+/**
+  * Extract the data of a year from a given dataset.
+  *
+  * @param {number} year - Year in YYYY format.
+  * @param {Array} dataset
+  *
+  * @returns {Array}
+**/
+const dataOfYear = (year = (new Date().getFullYear()), dataset = tempsPerDay) => {
+  console.log(dataset);
+
+  return dataset.filter(day => day.date.startsWith(year));
+};
+
+/**
+  * Create object from array with dataset per year.
+  *
+  * @param {Function} fn - Get the data of a year.
+  *
+  * @returns {Object}
+**/
+const splitDatasetIntoYears = (fn = dataOfYear) => {
+  let perYearObject = {};
+
+  for (let year = 1931; year < 2020; year++) {
+    perYearObject[`year${year}`] = {
+      year,
+      dataset: fn(year),
+    };
+  }
+
+  return perYearObject;
+};
+
+/**
+  * Get the first spring day.
+  *
+  * @param {Array} dataset
+  *
+  * @returns {Date|string} Date of first spring day or a string if it was an unusually cold year (improbable).
+**/
+const firstSpringDayForYear = (tempsInYear) => {
+  const filtered = tempsInYear.filter(day => day.maxC >= 15);
+
+  return filtered.length > 0 ? new Date(filtered[0].date) : "It was a very cold year... No days with temperatures above 15 degrees celsius! 0_o";
+};
+
+const firstSpringDayPerEachYear = (yearsObj) => {
+  return Object.values(yearsObj).map(({ dataset }) => {
+    return firstSpringDayForYear(dataset);
+  });
+};
+
+/**
+  * Log the result in a Dutch sentence.
+  *
+  * @param {Date} result - Date of the first spring day.
+  *
+  * @returns {string}
+**/
+const logResult = (result) => {
+  return `De eerste lentedag van ${result.getFullYear()} was ${result.toLocaleDateString("nl-NL")}`;
+};
+
+/**
+  * Draw the graph and write the logs (side effects)
+  *
+  * @todo Break this monster function up...
+**/
+const drawResults = () => {
+  const ZOOM_MULTIPLIER = 4;
+
   const $barchart = document.querySelector("#barchart");
   const $axisX = document.querySelector("#axisX");
   const $axisY = document.querySelector("#axisY");
   const $resultsLog = document.querySelector("#resultsLog");
 
-  /**
-    * Array with objects: one for each day from january 1931 up to january 2020.
-    *
-    * Each object is/should be formatted as follows:
-    *
-    * {
-    *   "date": "1931-01-01",
-    *
-    *   // The third number refers to the Dutch "decade": a period of ten days (confusing? ¯\_(ツ)_/¯):
-    *   "yearMonthTenDays": "1931m01d01",
-    *
-    *   // Maximum temperature in degrees Celsius of that day:
-    *   "maxC": 5.8
-    * },  
-  **/
-  const _tempsPerDay = data;
+  const lineStyles = { margin: "0 1px", width: "8px", height: 0 };
+  const getFirstSpringDayPerYear = firstSpringDayPerEachYear(splitDatasetIntoYears(dataOfYear));
 
-  /**
-    * Get the number of days in a given month.
-    *
-    * @param {number} month - Month in MM format (use current month if undefined).
-    * @param {number} year - Year in YYYY format (use current year if undefined).
-    *
-    * @returns {number}
-  **/
-  const daysInMonth = (year = (new Date().getFullYear()), month = (new Date().getMonth())) => {
-    return new Date(year, month, 0).getDate();
+  // @todo Create table with data ("Number of days since January 1st" and "Date of first spring day" in two columns)
+  // const nodeTable = document.createElement("table");
+  // const nodeThead = document.createElement("thead");
+  // const nodeTh = document.createElement("th");
+  // const nodeTh1 = nodeTh;
+  // const nodeTh2 = nodeTh;
+
+  let lineData = [];
+  let axisYDays = 0;
+
+  // nodeTh1.innerHTML = "DAGEN T/M";
+  // nodeTh1.innerHTML = "DATUM";
+  // nodeThead.innerHTML = nodeTh;
+  // nodeTable.innerHTML = nodeThead;
+
+  // Make and append a new bar for each data point.
+  for (let index = 0; index < getFirstSpringDayPerYear.length; index++) {
+    const firstSpringDay = getFirstSpringDayPerYear[index];
+
+    const nodeLine = document.createElement("hr");
+    const nodeYear = document.createElement("span");
+    const nodeLog = document.createElement("p");
+    const nodeTr = document.createElement("tr");
+    const nodeDataTd = document.createElement("td");
+
+    const year = firstSpringDay.getFullYear();
+    const month = firstSpringDay.getMonth();
+
+    let value = 0;
+
+    for (let index = month; index > 0; index--) {
+      value = value + daysInMonth(year, index);
+    }
+
+    value = value + firstSpringDay.getDate();
+
+    axisYDays = value >= axisYDays ? value + 10 : axisYDays;
+
+    // Add the value of the bar to an array.
+    lineData.push({ 
+      year: firstSpringDay.getFullYear(),
+      value,     
+    });
+
+    // Apply styling to bar.
+    for (const style in lineStyles) {
+      nodeLine.style[style] = lineStyles[style];
+    }
+
+    nodeLine.style.height = 0;
+    nodeLine.setAttribute("title", value);
+    nodeLine.setAttribute("data-year", lineData[index].year);
+
+    setTimeout(() => {
+      nodeLine.style.height = `${value * ZOOM_MULTIPLIER}px`;
+    }, 500);
+
+    const twoDigitYear = lineData[index].year - (lineData[index].year >= 2000 ? 2000 : 1900);
+    nodeYear.innerHTML = `'${twoDigitYear}`;
+    $axisX.appendChild(nodeYear);
+
+    // Log result to page.
+    nodeLog.innerHTML = logResult(firstSpringDay);
+    nodeDataTd.innerHTML = `${value}, ${firstSpringDay.toLocaleDateString("nl-NL")}`;
+    $resultsLog.appendChild(nodeLog);
+    $resultsLog.appendChild(nodeDataTd);
+
+    // Add the bar to the chart.
+    $barchart.appendChild(nodeLine);
   }
 
-  /**
-    * Extract the data of a year from a given dataset.
-    *
-    * @param {number} year - Year in YYYY format.
-    * @param {Array} dataset
-    *
-    * @returns {Array}
-  **/
-  const dataOfYear = (year = (new Date().getFullYear()), dataset = _tempsPerDay) => {
-    return dataset.filter(day => day.date.startsWith(year));
-  };
+  $barchart.style.width = `${getFirstSpringDayPerYear.length}px;`;
+  $barchart.style.opacity = 1;
+  $barchart.style.setProperty("--barchart-height", `${axisYDays * ZOOM_MULTIPLIER}px`);
 
-  /**
-    * Create object from array with dataset per year.
-    *
-    * @param {Function} fn - Get the data of a year.
-    *
-    * @returns {Object}
-  **/
-  const splitDatasetIntoYears = (fn = dataOfYear) => {
-    let perYearObject = {};
-
-    for (let year = 1931; year < 2020; year++) {
-      perYearObject[`year${year}`] = {
-        year,
-        dataset: fn(year),
-      };
-    }
-
-    return perYearObject;
-  };
-
-  /**
-    * Get the first spring day.
-    *
-    * @param {Array} dataset
-    *
-    * @returns {Date|string} Date of first spring day or a string if it was an unusually cold year (improbable).
-  **/
-  const firstSpringDayForYear = (tempsInYear) => {
-    const filtered = tempsInYear.filter(day => day.maxC >= 15);
-
-    return filtered.length > 0 ? new Date(filtered[0].date) : "It was a very cold year... No days with temperatures above 15 degrees celsius! 0_o";
-  };
-
-  const firstSpringDayPerEachYear = (yearsObj) => {
-    return Object.values(yearsObj).map(({ dataset }) => {
-      return firstSpringDayForYear(dataset);
-    });
-  };
-
-  /**
-    * Log the result in a Dutch sentence.
-    *
-    * @param {Date} result - Date of the first spring day.
-    *
-    * @returns {string}
-  **/
-  const logResult = (result) => {
-    return `De eerste lentedag van ${result.getFullYear()} was ${result.toLocaleDateString("nl-NL")}`;
-  };
-
-  /**
-    * Draw the graph and write the logs (side effects)
-    *
-    * @todo Break this monster function up...
-  **/
-  const drawResults = () => {
-    const ZOOM_MULTIPLIER = 4;
-
-    const lineStyles = { margin: "0 1px", width: "8px", height: 0 };
-    const getFirstSpringDayPerYear = firstSpringDayPerEachYear(splitDatasetIntoYears(dataOfYear));
-
-    // @todo Create table with data ("Number of days since January 1st" and "Date of first spring day" in two columns)
-    // const nodeTable = document.createElement("table");
-    // const nodeThead = document.createElement("thead");
-    // const nodeTh = document.createElement("th");
-    // const nodeTh1 = nodeTh;
-    // const nodeTh2 = nodeTh;
-
-    let lineData = [];
-    let axisYDays = 0;
-
-    // nodeTh1.innerHTML = "DAGEN T/M";
-    // nodeTh1.innerHTML = "DATUM";
-    // nodeThead.innerHTML = nodeTh;
-    // nodeTable.innerHTML = nodeThead;
-
-    // Make and append a new bar for each data point.
-    for (let index = 0; index < getFirstSpringDayPerYear.length; index++) {
-      const firstSpringDay = getFirstSpringDayPerYear[index];
-
-      const nodeLine = document.createElement("hr");
-      const nodeYear = document.createElement("span");
-      const nodeLog = document.createElement("p");
-      const nodeTr = document.createElement("tr");
-      const nodeDataTd = document.createElement("td");
-
-      const year = firstSpringDay.getFullYear();
-      const month = firstSpringDay.getMonth();
-
-      let value = 0;
-
-      for (let index = month; index > 0; index--) {
-        value = value + daysInMonth(year, index);
-      }
-
-      value = value + firstSpringDay.getDate();
-
-      axisYDays = value >= axisYDays ? value + 10 : axisYDays;
-
-      // Add the value of the bar to an array.
-      lineData.push({ 
-        year: firstSpringDay.getFullYear(),
-        value,     
-      });
-
-      // Apply styling to bar.
-      for (const style in lineStyles) {
-        nodeLine.style[style] = lineStyles[style];
-      }
-
-      nodeLine.style.height = 0;
-      nodeLine.setAttribute("title", value);
-      nodeLine.setAttribute("data-year", lineData[index].year);
-
-      setTimeout(() => {
-        nodeLine.style.height = `${value * ZOOM_MULTIPLIER}px`;
-      }, 500);
-
-      const twoDigitYear = lineData[index].year - (lineData[index].year >= 2000 ? 2000 : 1900);
-      nodeYear.innerHTML = `'${twoDigitYear}`;
-      $axisX.appendChild(nodeYear);
-
-      // Log result to page.
-      nodeLog.innerHTML = logResult(firstSpringDay);
-      nodeDataTd.innerHTML = `${value}, ${firstSpringDay.toLocaleDateString("nl-NL")}`;
-      $resultsLog.appendChild(nodeLog);
-      $resultsLog.appendChild(nodeDataTd);
-
-      // Add the bar to the chart.
-      $barchart.appendChild(nodeLine);
-    }
-
-    $barchart.style.width = `${getFirstSpringDayPerYear.length}px;`;
-    $barchart.style.opacity = 1;
-    $barchart.style.setProperty("--barchart-height", `${axisYDays * ZOOM_MULTIPLIER}px`);
-
-    $axisY.innerHTML = `<div>${axisYDays}</div> <div>0</div>`;
-  };
-
-  drawResults();
+  $axisY.innerHTML = `<div>${axisYDays}</div> <div>0</div>`;
 
   $barchart.addEventListener("mouseover", ({ target }) => {
     if (target && target.title) {
       console.log(`${target.dataset.year}: ${target.title}`);
     }
   });
-}());
+};
+
+(async function() {
+  try {
+    const weatherData = await getWeatherData();
+
+    return weatherData;
+  } catch (error) {
+    console.error(error);
+    alert("An unknown problem occured. Please try again by reloading the page.");
+  }
+})().then(data => {
+  console.log(data);
+
+  tempsPerDay = data;
+
+  drawResults();
+});
